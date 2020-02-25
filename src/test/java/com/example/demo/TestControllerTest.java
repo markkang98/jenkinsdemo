a9663d30e0a5763f96ada4d91c2253d15b20ca02
sk531 <sk531@duke.edu>
2020-02-24 20:33:37 -0500

    first commit

---
+++ src/test/java/com/example/demo/TestControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestControllerTest {
+
+    @Test
+    public void test(){
+        TestController testController = new TestController();
+
+        assertEquals(testController.test(), "Hello World");
+    }
+
+}
